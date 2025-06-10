62dad89783b087af869a827378f4df8785f3603a
Myroslava Volosko (IZB Meister; MSc student) <myroslava.volosko@gmail.com>
2025-06-10 11:50:58 +0200

    Revert "updated scripts"
    This reverts commit 665c796410acbb6781af7c4a6000e134d69ff921.

--- scripts/nuclei_segmentation.sh
+++ scripts/nuclei_segmentation.sh
@@ -3,38 +3,17 @@
 #SBATCH --output=NUCLEI_SEGM_%j.out
 #SBATCH --error=NUCLEI_SEGM_%j.err
 #SBATCH --time=04:00:00
-#SBATCH --mem=124G
+#SBATCH --mem=32G
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:1
 
 set -euo pipefail
-#--gres=gpu:1
-#--partition=all -w izbdodoma
 
 # ——— User parameters ———
-#High res images - nuclei size needs to be changed in segment.py so far, be careful of memory requirements,
-# might need to use --mem=124G and wait for resource allocation
-RAW_DIR="/mnt/external.data/MeisterLab/mvolosko/image_project/high_res_images/1273/"
-DENOISED_DIR="None"
+RAW_DIR="/mnt/external.data/MeisterLab/Dario/SDC1/1273/20241108_e_hs"
+DENOISED_DIR="${RAW_DIR}/N2V_sdc1_dpy27_mSG_emr1_mCh/denoised"
 MODEL_DIR="/mnt/external.data/MeisterLab/lhinder/segmentation_3d_anja/code/worms_1000epochs_v0"
-OUT_ROOT="/mnt/external.data/MeisterLab/mvolosko/image_project/SDC1/1273_fast_imaging/"
-
-# RAW_DIR="/mnt/external.data/MeisterLab/mvolosko/image_project/high_res_images/1268/"
-# DENOISED_DIR="None"
-# MODEL_DIR="/mnt/external.data/MeisterLab/lhinder/segmentation_3d_anja/code/worms_1000epochs_v0"
-# OUT_ROOT="/mnt/external.data/MeisterLab/mvolosko/image_project/SDC1/1268_fast_imaging/"
-
-#Dario Single images
-# RAW_DIR='/mnt/external.data/MeisterLab/Dario/SDC1/1273/20240813_e/'
-# MODEL_DIR="/mnt/external.data/MeisterLab/lhinder/segmentation_3d_anja/code/worms_1000epochs_v0"
-# DENOISED_DIR=="${RAW_DIR}/N2V_dpy27_mSG_emr1_mCh/denoised/"
-# OUT_ROOT="/mnt/external.data/MeisterLab/mvolosko/image_project/SDC1/1273/20240813_e/"
-
-#Dario images heatshock timelapse
-# RAW_DIR="/mnt/external.data/MeisterLab/Dario/SDC1/1273/20241108_e_hs"
-# DENOISED_DIR="${RAW_DIR}/N2V_sdc1_dpy27_mSG_emr1_mCh/denoised"
-# MODEL_DIR="/mnt/external.data/MeisterLab/lhinder/segmentation_3d_anja/code/worms_1000epochs_v0"
-# OUT_ROOT="/mnt/external.data/MeisterLab/mvolosko/image_project/SDC1/1273/20241108_e_hs"
+OUT_ROOT="/mnt/external.data/MeisterLab/mvolosko/image_project/SDC1/1273/20241108_e_hs"
 
 # RAW_DIR="/mnt/external.data/MeisterLab/Dario/SDC1/1273/20241010_e_tl/"
 # DENOISED_DIR="${RAW_DIR}/N2V_sdc1_dpy27_mSG_emr1_mCh/denoised"
@@ -50,7 +29,6 @@ OUT_ROOT="/mnt/external.data/MeisterLab/mvolosko/image_project/SDC1/1273_fast_im
 # DENOISED_DIR="${RAW_DIR}/N2V_sdc1_dpy27_mSG_emr1_mCh/denoised"
 # MODEL_DIR="/mnt/external.data/MeisterLab/lhinder/segmentation_3d_anja/code/worms_1000epochs_v0"
 # OUT_ROOT="/mnt/external.data/MeisterLab/mvolosko/image_project/DPY27/1268/20241010_e_tl/"
-
 cd ../
 # activate env
 set +u
@@ -61,24 +39,22 @@ set -u
 mkdir -p "${OUT_ROOT}"
 
 # ——— 1. Segment & EDT (use raw as fallback if no denoised) ———
-# python -m segmentation.cli segment \
-#     --raw-dir      "${RAW_DIR}" \
-#     --denoised-dir "${DENOISED_DIR}" \
-#     --pattern      "*.nd2" \
-#     --model        "${MODEL_DIR}" \
-#     --out-root     "${OUT_ROOT}" \
-#     --gpu \
-#     --do-qc
+python -m segmentation.cli segment \
+    --raw-dir      "${RAW_DIR}" \
+    --denoised-dir "${DENOISED_DIR}" \
+    --pattern      "*.nd2" \
+    --model        "${MODEL_DIR}" \
+    --out-root     "${OUT_ROOT}" \
+    --gpu \
+    --do-qc
 
-# with no denoising step:
+# if you also have .czi images with no denoising step:
 python -m segmentation.cli segment \
     --raw-dir  "${RAW_DIR}" \
-    --pattern  "*.tif" \
+    --pattern  "*.czi" \
     --model    "${MODEL_DIR}" \
     --out-root "${OUT_ROOT}" \
-    --gpu \
-    --do-qc \
-    --blur-sigma "2.0"
+    --gpu
 
 # ——— 2. Distance–intensity analysis & summarization ———
 python -m segmentation.cli analyze \
@@ -86,4 +62,4 @@ python -m segmentation.cli analyze \
     --seg-dir  "${OUT_ROOT}/segmentation" \
     --out-root "${OUT_ROOT}"
 
-echo "✅ Pipeline completed – results in ${OUT_ROOT}"
+echo "✅ Pipeline complete – results in ${OUT_ROOT}"
--- scripts/spot_detection.sh
+++ scripts/spot_detection.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=SPOT_det
-#SBATCH --output=SPOT_det_%j.out
-#SBATCH --error=SPOT_det_%j.err
-#SBATCH --time=2:00:00
+#SBATCH --job-name=spot_analysis
+#SBATCH --output=spot_analysis_%j.out
+#SBATCH --error=spot_analysis_%j.err
+#SBATCH --time=04:00:00
 #SBATCH --mem=64G
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1
-#SBATCH --partition=all
 
 ##### set all your dirs here #####
 
@@ -32,59 +31,18 @@
 # NUCLEI_CSV_DIR="${BASE_DIR}/nuclei/"
 # OUTPUT_DIR="${BASE_DIR}/spots/"
 
-# RAW_DIR='/mnt/external.data/MeisterLab/Dario/SDC1/1273/20241010_e_tl/'
-# BASE_DIR="/mnt/external.data/MeisterLab/mvolosko/image_project/SDC1/1273/20241010_e_tl/"
-# INPUT_DIR="${RAW_DIR}/N2V_dpy27_mSG_emr1_mCh/denoised/"
-# NUCLEI_MASK_DIR="${BASE_DIR}/segmentation/"
-# NUCLEI_CSV_DIR="${BASE_DIR}/nuclei/"
-# OUTPUT_DIR="${BASE_DIR}/spots/"
-
-#Single Images
-# RAW_DIR='/mnt/external.data/MeisterLab/Dario/SDC1/1273/20240813_e/'
-# BASE_DIR="/mnt/external.data/MeisterLab/mvolosko/image_project/SDC1/1273/20240813_e/"
-# INPUT_DIR="${RAW_DIR}/N2V_sdc1_dpy27_mSG_emr1_mCh/denoised/"
-# NUCLEI_MASK_DIR="${BASE_DIR}/segmentation/"
-# NUCLEI_CSV_DIR="${BASE_DIR}/nuclei/"
-# OUTPUT_DIR="${BASE_DIR}/spots/"
-
-# RAW_DIR='/mnt/external.data/MeisterLab/Dario/DPY27/1268/20240808_e/'
-# BASE_DIR="/mnt/external.data/MeisterLab/mvolosko/image_project/dpy27/DPY27/1268/20240808_e/"
-# INPUT_DIR="${RAW_DIR}/N2V_dpy27_mSG_emr1_mCh/denoised/"
-# NUCLEI_MASK_DIR="${BASE_DIR}/segmentation/"
-# NUCLEI_CSV_DIR="${BASE_DIR}/nuclei/"
-# OUTPUT_DIR="${BASE_DIR}/spots/"
-
-#High res images
-
-# Test small subset of images 1268
-RAW_DIR="/mnt/external.data/MeisterLab/mvolosko/image_project/high_res_images/1268/"
-BASE_DIR="/mnt/external.data/MeisterLab/mvolosko/image_project/SDC1/test_subset/"
-INPUT_DIR="${RAW_DIR}"
+RAW_DIR='/mnt/external.data/MeisterLab/Dario/SDC1/1273/20241010_e_tl/'
+BASE_DIR="/mnt/external.data/MeisterLab/mvolosko/image_project/SDC1/1273/20241010_e_tl/"
+INPUT_DIR="${RAW_DIR}/N2V_dpy27_mSG_emr1_mCh/denoised/"
 NUCLEI_MASK_DIR="${BASE_DIR}/segmentation/"
 NUCLEI_CSV_DIR="${BASE_DIR}/nuclei/"
 OUTPUT_DIR="${BASE_DIR}/spots/"
-
-# Need the adjustments in the pixel size for this images, otherwise it takes too long and probably looks at wrong spots
-# RAW_DIR="/mnt/external.data/MeisterLab/mvolosko/image_project/high_res_images/1268/"
-# BASE_DIR="/mnt/external.data/MeisterLab/mvolosko/image_project/SDC1/1268_fast_imaging/"
-# INPUT_DIR="${RAW_DIR}"
-# NUCLEI_MASK_DIR="${BASE_DIR}/segmentation/"
-# NUCLEI_CSV_DIR="${BASE_DIR}/nuclei/"
-# OUTPUT_DIR="${BASE_DIR}/spots/"
-
-# RAW_DIR="/mnt/external.data/MeisterLab/mvolosko/image_project/high_res_images/1273/"
-# BASE_DIR="/mnt/external.data/MeisterLab/mvolosko/image_project/SDC1/1273_fast_imaging/"
-# INPUT_DIR="${RAW_DIR}"
-# NUCLEI_MASK_DIR="${BASE_DIR}/segmentation/"
-# NUCLEI_CSV_DIR="${BASE_DIR}/nuclei/"
-# OUTPUT_DIR="${BASE_DIR}/spots/"
-##################################
+###################################
 
 
-# make sure the output directories exist
+# make sure the output directory exists
 mkdir -p "$OUTPUT_DIR"
 mkdir -p "$BASE_DIR"
-
 # load your conda env
 source /home/mvolosko/miniconda3/etc/profile.d/conda.sh
 conda activate lhcellpose
